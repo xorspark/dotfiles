c83c6b0c79e6e799ec9339632b43cd0bba389c76
Anthony S. Piris <anthonypiris@gmail.com>
2025-02-22 12:20:12 -0500

    tmux fixes, add config to make wezterm tmux-like when working locally, along with nvim integration

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,6 +1,14 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+config.term = "xterm-256color"
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	top = 0,
+	bottom = 0,
+	left = 0,
+	right = 0,
+}
 config.color_scheme = "Numix Darkest (terminal.sexy)"
 config.font = wezterm.font("Spleen 16x32")
 config.font_size = 16
@@ -19,4 +27,129 @@ config.visual_bell = {
 	fade_out_duration_ms = 150,
 }
 config.audible_bell = "Disabled"
+
+-- tmux-like stuff
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+
+local action = wezterm.action
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "CTRL|SHIFT" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "CTRL|SHIFT" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 5 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+config.keys = {
+
+	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
+	{
+		key = "\\",
+		mods = "LEADER",
+		action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "m",
+		mods = "LEADER",
+		action = action.TogglePaneZoomState,
+	},
+	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+	{
+		key = "c",
+		mods = "LEADER",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+
+	{
+		key = "p",
+		mods = "LEADER",
+		action = action.ActivateTabRelative(-1),
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = action.ActivateTabRelative(1),
+	},
+	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	{
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+	{
+		key = "9",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|TABS|WORKSPACES" }),
+	},
+	{
+		key = "W",
+		mods = "CTRL|SHIFT",
+		action = action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+}
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = action.ActivateTab(i - 1),
+	})
+end
+
 return config
--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -3,8 +3,8 @@ return {
     "stevearc/conform.nvim",
     event = "BufWritePre", -- uncomment for format on save
     config = function()
-      require("configs.conform")
-    end
+      require "configs.conform"
+    end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
@@ -19,9 +19,38 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "vim", "lua", "vimdoc", "html", "css", "c", "markdown", "markdown_inline", "javascript"
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "c",
+        "markdown",
+        "markdown_inline",
+        "javascript",
       },
-      auto_install = true
+      auto_install = true,
     },
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
+    config = function()
+      local smart_splits = require "smart-splits"
+      smart_splits.setup {
+        default_amount = 5,
+      }
+      local keymap = vim.keymap
+
+      keymap.set("n", "<C-h>", smart_splits.move_cursor_left)
+      keymap.set("n", "<C-j>", smart_splits.move_cursor_down)
+      keymap.set("n", "<C-k>", smart_splits.move_cursor_up)
+      keymap.set("n", "<C-l>", smart_splits.move_cursor_right)
+
+      keymap.set("n", "<C-S-h>", smart_splits.resize_left)
+      keymap.set("n", "<C-S-j>", smart_splits.resize_down)
+      keymap.set("n", "<C-S-k>", smart_splits.resize_up)
+      keymap.set("n", "<C-S-l>", smart_splits.resize_right)
+    end,
   },
 }

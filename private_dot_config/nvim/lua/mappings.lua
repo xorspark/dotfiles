b5f0b925b596b32d22a11e119029e89d3c9b4fd8
Anthony S. Piris <anthony.piris@getcandidly.com>
2025-03-14 11:40:26 -0400

    Map double escape to enter normal mode inside embedded vim terminal

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -6,5 +6,5 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("t", "<Esc><Esc>", [[<C-\><C-n>]])
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

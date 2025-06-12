5f96d37fb88912e294e8dca5cb82058197252d49
Anthony S. Piris <anthony.piris@getcandidly.com>
2025-06-12 14:03:14 -0400

    enable ruff lsp

--- private_dot_config/nvim/lua/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "basedpyright", "ts_ls", "eslint" }
+local servers = { "html", "cssls", "basedpyright", "ts_ls", "eslint", "intelephense", "ruff" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
